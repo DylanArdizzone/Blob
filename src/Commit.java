5f418159ded98ca1ffdb876ef7b89897d56407e7
catherine cho <ccho2@hwemail.com>
2022-09-19 12:16:56 -0700

    added the SHA1 method to Commit

--- src/Commit.java
+++ src/Commit.java
@@ -4,6 +4,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;  
 import java.util.Calendar;  
 import java.io.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 public class Commit {
 	private static String parent = null;
 	private static String child = null;
@@ -18,12 +21,46 @@ public class Commit {
 		parent = pointer;
 		
 	}
+	private static String changeSha(String input)
+	{
+		try {
+			// getInstance() method is called with algorithm SHA-1
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+			// digest() method is called
+			// to calculate message digest of the input string
+			// returned as array of byte
+			byte[] messageDigest = md.digest(input.getBytes());
+
+			// Convert byte array into signum representation
+			BigInteger no = new BigInteger(1, messageDigest);
+
+			// Convert message digest into hex value
+			String hashtext = no.toString(16);
+
+			// Add preceding 0s to make it 32 bit
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+
+			// return the HashText
+			return hashtext;
+		}
+
+		// For specifying wrong message digest algorithms
+		catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
 	public static String getDate() {
 		// from javatpoint
 		Date date = Calendar.getInstance().getTime();  
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
 		String strDate = dateFormat.format(date);  
 		return strDate; 
+	}
+	public static void writesFiles() {
+		
 	}
 	
 }
