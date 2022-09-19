a2a23e5dc79bc09507c06ad455cd95fa39a88e58
catherine cho <ccho2@hwemail.com>
2022-09-19 12:10:10 -0700

    created Commit and added parent and child
    process of coding the constructor

--- src/Commit.java
+++ src/Commit.java
@@ -1,5 +1,20 @@
 import java.util.*;
 import java.io.*;
 public class Commit {
+	private static String parentCommit = null;
+	private static String otherCommit = null;
+	private static String pTree; 
+	private static String author;
+	private static String summary;
+	private static String date;
+	public Commit(String pt, String summ, String a, String pointer) {
+		pTree = pt;
+		summary = summ;
+		author = a;
+		
+	}
+	public static String getDate() {
+		return date;
+	}
 	
 }
