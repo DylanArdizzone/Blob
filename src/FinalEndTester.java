35fb355bffbbda22be8a87b3989b6fa8249e11b1
DylanArdizzone <dardizzone1@hwemail.com>
2022-09-28 21:04:22 -0700

    problems located

--- src/FinalEndTester.java
+++ src/FinalEndTester.java
@@ -6,14 +6,14 @@ public class FinalEndTester {
 	public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException, IOException {
 		Index i = new Index();
 		i.add("testAnotherText.txt");
-	//	i.add("superMario.txt");
 		i.add("textTesterToTest.txt");
 		Commit comm = new Commit("this is a commit", "me :)", null);
-//		System.out.println(comm.getFileName());
+		System.out.println(comm.getFileName());
 		i.add("superMario.txt");
 		i.add("superMarioBrothers2.txt");
-		Commit comm2 = new Commit("second commit", "me again :)", comm.getFileName());
-		comm.setChild(comm2);
+		System.out.println(comm.getFileName());
+		Commit comm2 = new Commit("second commit", "me again :)", comm.getFileName()); //second commit's tree contains the contents of the first commit can't have that 
+		//comm.setChild(comm2); //setting child changes sha1code no longer points to real filename
 	}
 
 }
